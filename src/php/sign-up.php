8370a0b8fcfa6d550cc6bb4c2c1c34f43770dc03
Efren <efrengutierrez@gmail.es>
2024-12-07 19:12:55 +0100

    Comienzo a implementar el sing-up.php(funciona bien)

--- src/php/sign-up.php
+++ src/php/sign-up.php
@@ -1,12 +1,9 @@
 <?php
 $usuario = $_POST['user'];
 $nombre = $_POST['name'];
+$apellido1 = $_POST['surname1'];
 $apellido2 = $_POST['surname2'];
-$contrasena = $_POST['pass']
+$contrasena = $_POST['pass'];
 
-function usuario(){
-    echo "usuario: " . $usuario . " nombre: " . $nombre ; 
-}
 
-usuario();
 ?>
