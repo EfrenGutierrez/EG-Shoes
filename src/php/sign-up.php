e5491dbbc4cd036ab829a55b349bfedb8f68fbac
Efren <efrengutierrez@gmail.es>
2024-12-08 12:58:14 +0100

    se vuelve a crear la conexion y la base de datos

--- src/php/sign-up.php
+++ src/php/sign-up.php
@@ -11,6 +11,26 @@ $serv_name = "localhost";
 $username = "root";
 $serv_pass = "";
 $serv_DB = "egshoes";
+
 //se crea la conexion con el servidor
+
+$con = new mysqli($serv_name,$username,$serv_pass);
+
+if($con->connect_error){
+    echo "conexion fallida";
+}
+else{
+    echo "conexionn exitosa";
+}
+
+$sql = "CREATE DATABASE egshoes";
+if($con->query($sql)===true){
+    echo "DB creada con exito";
+}
+else{
+    echo "error al crear la DB " . $con->error;
+}
+
+$con->close();
 
 ?>
