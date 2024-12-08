5d06a76f5e17478cb6b2515fbef6bb8e43827801
Efren <efrengutierrez@gmail.es>
2024-12-08 19:06:21 +0100

    comenzamos la conexion con PDO

--- src/php/sign-up.php
+++ src/php/sign-up.php
@@ -12,25 +12,9 @@ $username = "root";
 $serv_pass = "";
 $serv_DB = "egshoes";
 
-//se crea la conexion con el servidor
+//se crea la conexion con el servidor con PDO
 
-$con = new mysqli($serv_name,$username,$serv_pass);
 
-if($con->connect_error){
-    echo "conexion fallida";
-}
-else{
-    echo "conexionn exitosa";
-}
 
-$sql = "CREATE DATABASE egshoes";
-if($con->query($sql)===true){
-    echo "DB creada con exito";
-}
-else{
-    echo "error al crear la DB " . $con->error;
-}
-
-$con->close();
 
 ?>
